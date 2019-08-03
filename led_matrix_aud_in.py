f16ac3ba36f59d6ed4409b6ad0f5e2485e127b31
james <j.gleave.james@gmail.com>
2019-08-03 12:48:57 -0700

    Added util

--- led_matrix_aud_in.py
+++ led_matrix_aud_in.py
@@ -15,14 +15,14 @@ note: with 2048 samples per chunk, I'm getting 20FPS
 when also running the spectrum, its about 15FPS
 """
 
-from rgbmatrix import RGBMatrix, RGBMatrixOptions
+# from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
-options = RGBMatrixOptions()
-options.rows = 32
-options.cols = 64
-options.chain_length = 1
-options.parallel = 1
-options.hardware_mapping = 'adafruit-hat'
+# options = RGBMatrixOptions()
+# options.rows = 32
+# options.cols = 64
+# options.chain_length = 1
+# options.parallel = 1
+# options.hardware_mapping = 'adafruit-hat'
 
 import pyaudio
 import os
@@ -40,7 +40,6 @@ RATE = 44100                 # samples per second
 
 
 class Spectrogram:
-
     def __init__(self):
         self.y_val = []
         self.stream = None
@@ -111,8 +110,8 @@ class Spectrogram:
             counter += 1
         clear(bar_y)
         self.update_matrix()
-        
-        
+
+
 class Waveform:
     def __init__(self):
         self.Dmatrix = None
@@ -138,36 +137,86 @@ class Waveform:
             channels=CHANNELS,
             rate=RATE,
             frames_per_buffer=CHUNK,
-            input_device_index=1,
+            input_device_index=0,
             input=True,
         )
         
-        self.Dmatrix = RGBMatrix(options=options)
+        # self.Dmatrix = RGBMatrix(options=options)
 
         print('stream started')
 
-    def terminate(self):
+    def terminate(self, p):
         p.terminate()
 
-        
+
+class AudioStream:
+    """
+    Audio gets both the fft and waveform
+    simultaneously
+    """
+    def __init__(self):
+        self.spectrum = []
+        self.peak = 0
+        self.stream = None
+        self.Dmatrix = None
+        self.start()
+
+    def start(self):
+        # pyaudio class instance
+        p = pyaudio.PyAudio()
+
+        # stream object to get data from microphone
+        self.stream = p.open(
+            format=FORMAT,
+            channels=CHANNELS,
+            rate=RATE,
+            frames_per_buffer=CHUNK,
+            input_device_index=0,
+            input=True,
+        )
+
+        # self.Dmatrix = RGBMatrix(options=options)
+
+        print('stream started')
+
+    def update(self):
+        spectrum_values = []
+
+        # binary data
+        data = np.fromstring(self.stream.read(CHUNK, exception_on_overflow=False), dtype=np.int16)
+        peak = np.average(np.abs(data)) * 2
+        self.peak = int(500 * peak / 2 ** 16)
+
+        # convert data to integers, make np array, then offset it by 127
+        data_int = struct.unpack(str(2 * CHUNK) + 'B', data)
+
+        # compute FFT and update line
+        yf = fft(data_int)
+        freq = (np.abs(yf[0:CHUNK]) / (128 * CHUNK))
+
+        split_number = 0  # determines the amount of different frequencies we average
+        ave_list = []
+
+        for n in freq:
+            ave_list.append(n)
+            if split_number % 64 == 0:
+                specific_spectrum_value = np.sum(ave_list, axis=0)
+                spectrum_values.append(specific_spectrum_value)
+
+                self.spectrum = spectrum_values[:]
+
+                ave_list = []
+                split_number += 1
+        clear(spectrum_values)
+        return self.peak, self.spectrum
+
+
 def sigmoid(x):
     return 1/(1 + pow(math.e, -x))
 
+
 def clear(list):
     del list[:]
-    
-def run_spectrogram(s):
-    while True:
-        s.display()
-    
-def run_waveform(w):
-    while True:
-        w.update()
 
-#s = Spectrogram()
-#s = run_spectrogram(s)
-#s = Waveform()
-#run_waveform(s)
-    
 
 

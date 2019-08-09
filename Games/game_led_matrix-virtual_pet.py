dd5b19c5f030426b752c699049388c729c80b98a
james <j.gleave.james@gmail.com>
2019-08-09 10:36:51 -0700

    added alot actually

---
+++ Games/game_led_matrix-virtual_pet.py
@@ -0,0 +1,16 @@
+import led_matrix_interface as Jworld
+import led_matrix_animator as animator
+
+
+class Pet:
+    """This class represents the pet itself. This will animate and store all information about the pet.
+        The pet eats physical pixels. It has health, hunger and mood."""
+    pass
+
+
+class Food:
+    pass
+
+
+class TimeTracker:
+    pass

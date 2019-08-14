ad509bf7ec5b0f65cf81ad25334504ac8022f361
jamesgleave <j.gleave.james@gmail.com>
2019-08-14 11:21:43 -0700

    updated util

--- led_matrix_util.py
+++ led_matrix_util.py
@@ -1,4 +1,9 @@
 """Utilities for the led library"""
+import subprocess
+
+
+def shutdown():
+    subprocess.call(["sudo", "poweroff"])
 
 
 def clamp(n, min_n, max_n):
@@ -12,6 +17,8 @@ def change_matrix_options(matrix_options, **kwargs):
     try:
         if kwargs.get("led_gpio_mapping") is not None:
             options.hardware_mapping = kwargs.get("led_gpio_mapping")
+        if kwargs.get("led_pwm_dither_bits") is not None:
+            options.led_pwm_dither_bits = kwargs.get("led_pwm_dither_bits")
         if kwargs.get("hardware_mapping") is not None:
             options.hardware_mapping = kwargs.get("hardware_mapping")
         if kwargs.get("multiplexing") is not None:
@@ -30,9 +37,28 @@ def change_matrix_options(matrix_options, **kwargs):
             options.led_rgb_sequence = kwargs.get("led_rgb_sequence")
         if kwargs.get("led_show_refresh"):
             options.show_refresh_rate = 1
-        if  kwargs.get("led_slowdown_gpio") is not None:
+        if kwargs.get("led_slowdown_gpio") is not None:
             options.gpio_slowdown = kwargs.get("led_slowdown_gpio")
-        if kwargs.get("led_no_hardware_pulse"):
-            options.disable_hardware_pulsing = True
-    except:
+        if kwargs.get("led_scan_mode") is not None:
+            options.led_scan_mode = kwargs.get("led_scan_mode")
+        if kwargs.get("led_daemon") is not None:
+            options.led_daemon = kwargs.get("led_daemon")
+
+    except Exception:
+        led_matrix_help()
         raise UserWarning("There was an error processing these commands")
+
+
+def led_matrix_help():
+    print("HELP:")
+    print("pwm_bits=<1..11> default is 11 -> Higher values lead to higher color range")
+    print("brightness=<percent> default is 100")
+    print("led_show_refresh Shows refresh rate of the led panel")
+    print("led_scan_mode=<0..1> 0 = progressive; 1 = interlaced (Default: 0) -> 1 looks better with low refresh rate")
+    print("pwm_lsb_nanoseconds PWM Nanoseconds for LSB (Default: 130) -> lower results in (maybe) more ghosting")
+    print("led_pwm_dither_bits Time dithering of lower bits (Default: 0) -> higher refresh rate but lower brightness")
+    print("led_slowdown_gpio=<0..2> Needed for faster Pis and/or slower panels (Default: 1) -> for pi3 2 may be needed")
+    print("led_daemon If this is set, the program puts itself into the background (running as 'daemon'). "
+          "You might want this if started from an init script at boot-time.")
+
+led_matrix_help()
